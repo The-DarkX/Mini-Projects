e4c42a6f76ed84ecd225a14efbf247ed2253b473
The-DarkX <mkaren051505@gmail.com>
2021-03-26 10:06:11 -0700

    Game 2 (Full), Game 1 (Partial), Game 3 Template

---
+++ Assets/Game 2/Scripts/CubeFloater.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFloater : MonoBehaviour
+{
+    public float movePower = 10f;
+
+    public Material blueMat;
+    public Material organgeMat;
+
+    Vector3 moveDirection;
+
+    bool isBlue = true;
+
+    MeshRenderer mr;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mr = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (isBlue)
+        {
+            moveDirection = Vector3.up;
+            mr.material = blueMat;
+        }
+        else 
+        {
+            moveDirection = Vector3.down;
+            mr.material = organgeMat;
+        }
+
+        rb.AddForce(moveDirection * movePower * Time.deltaTime, ForceMode.Force);
+    }
+
+	private void OnMouseDown()
+	{
+        isBlue = !isBlue;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        if (collision.transform.CompareTag("Ground") && !isBlue) 
+        {
+            Destroy(gameObject);
+
+        }
+	}
+}
---
+++ Assets/Game 2/Scripts/FloaterManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FloaterManager : MonoBehaviour
+{
+    public GameObject floaterPrefab;
+    public int totalAmount = 10;
+
+    public Vector2 spawnArea;
+
+    Vector3 spawnPos;
+    Quaternion spawnRotation;
+
+    int orangeCount;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+
+        for (int i = 0; i < totalAmount; i++)
+        {
+            spawnPos = new Vector3(Random.Range(-spawnArea.x, spawnArea.x), Random.Range(-spawnArea.y, spawnArea.y), transform.position.z);
+            spawnRotation = Quaternion.Euler(0, 0, Random.Range(0, 180));
+
+            Instantiate(floaterPrefab, spawnPos, spawnRotation, transform);
+        }
+    }
+
+    void Update()
+    {
+        if (transform.childCount < totalAmount) 
+        {
+            Time.timeScale = 0.5f;
+            Invoke("Restart", 0.5f);
+        }
+    }
+
+    void Restart() 
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index);
+    }
+
+	private void OnDrawGizmos()
+	{
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, spawnArea * 2);
+	}
+}
