cba0293c2a042d63a3e54a4a3f17d34220830cc2
NathanS525 <nathanscherrer2005@gmail.com>
2021-03-26 07:57:12 -0700

    Started Work on 2nd MiniProject

---
+++ Assets/Game 2/Scripts/PlatformRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRotate : MonoBehaviour
+{
+
+    public float rotationSpeed = 10.0f;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Game 2/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+
+    private Quaternion startingRotation;
+
+    private Vector3 startingPosition;
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+        startingRotation = transform.rotation;
+    }
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+        transform.rotation = startingRotation;
+
+        if (transform.position.y < -3)
+        {
+            transform.position = startingPosition;
+            transform.rotation = startingRotation;
+        }
+    }
+}
