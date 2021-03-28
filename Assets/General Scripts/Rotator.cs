acc62a9ef4f0dcc83a650ea383f4b6ad007e9c78
The-DarkX <mkaren051505@gmail.com>
2021-03-27 19:01:00 -0700

    Game 2 - Rotating obstacles

---
+++ Assets/General Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 20;
+
+    void Update()
+    {
+        transform.Rotate(transform.forward, speed * 10 * Time.deltaTime);
+    }
+}
