735962b4fd28df589f5738403bce870387659482
NathanS525 <nathanscherrer2005@gmail.com>
2021-03-31 13:50:45 -0700

    UI for game 4

---
+++ Assets/Game 4/Scripts/PlayerInputMainMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputMainMenu : MonoBehaviour
+{
+
+    public float xReset;
+    public float yReset;
+    public float xResetNeg;
+    public float yResetNeg;
+
+    public Rigidbody playerRb;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRb.AddRelativeForce(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRb.AddRelativeForce(-Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRb.AddRelativeForce(-Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRb.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
+        }
+
+
+        if (transform.position.y < yResetNeg)
+        {
+            transform.position = new Vector3(transform.position.x, yResetNeg, transform.position.z);
+        }
+        if (transform.position.y > yReset)
+        {
+            transform.position = new Vector3(transform.position.x, yReset, transform.position.z);
+        }
+        if (transform.position.x < xResetNeg)
+        {
+            transform.position = new Vector3(xResetNeg, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xReset)
+        {
+            transform.position = new Vector3(xReset, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Game 4/Scripts/ShooterScriptMainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScriptMainMenu : MonoBehaviour
+{
+
+    public float shooterSpeed = 10.0f;
+
+    public float highZBoundary;
+
+    public float lowZBoundary;
+
+    public bool direction;
+    
+    void Update()
+    {
+        if (transform.position.y > highZBoundary)
+        {
+            direction = false;
+        }
+        if (transform.position.y < lowZBoundary)
+        {
+            direction = true;
+        }
+
+        if (direction == true)
+        {
+            transform.Translate(Vector3.forward * shooterSpeed * Time.deltaTime);
+        }
+
+        if (direction == false)
+        {
+            transform.Translate(-Vector3.forward * shooterSpeed * Time.deltaTime);
+        }
+    }
+}
